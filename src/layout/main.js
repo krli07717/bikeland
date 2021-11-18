cd3862bb4eaeb2f6782e7ef30b1df98d5b359d0c
Ray Li <k.r.li07717@gmail.com>
2021-11-18 11:50:25 +0800

    feat: add locating message, adjust styles

--- src/layout/main.js
+++ src/layout/main.js
@@ -1,12 +1,14 @@
 import "./main.css";
 import bicycleWhiteSvg from "../assets/icon-bicycle-white.svg";
 import parkingSvg from "../assets/icon-parking.svg";
-import mapMarkerGreySvg from "../assets/icon-map-marker-grey.svg";
+import parkingRedSvg from "../assets/icon-parking-red.svg";
+import parkingGreySvg from "../assets/icon-parking-grey.svg";
 import collapseTopSvg from "../assets/icon-collapse-top.svg";
 import collapseDownSvg from "../assets/icon-collapse-down.svg";
 import sortSvg from "../assets/icon-sort.svg";
 import bicycle500Svg from "../assets/icon-bicycle-500.svg";
-import parkingRedSvg from "../assets/icon-parking-red.svg";
+import bicycleRedSvg from "../assets/icon-bicycle-red.svg";
+import bicycleGreySvg from "../assets/icon-bicycle-grey.svg";
 import geolocactionSvg from "../assets/icon-geolocation.svg";
 import userPositionMobileSvg from "../assets/icon-user-position-mobile.svg";
 import React, { useState, useEffect, useRef } from "react";
@@ -18,10 +20,9 @@ import L from "leaflet";
 function BikeMap(props) {
   console.log("bikemap");
   const bikeMapRef = useRef(null);
-  //   const [mapIsInititialized, setMapIsInitialized] = useState(false);
+  const userPositionMarkerRef = useRef(null);
   useEffect(() => {
     //   create map
-    // if (mapIsInititialized) return;
     if (bikeMapRef.current) return;
     console.log("creating map");
 
@@ -41,20 +42,32 @@ function BikeMap(props) {
 
   useEffect(() => {
     //  setView
-    if (!bikeMapRef.current) return;
+    if (!bikeMapRef.current) return; //no map
 
     console.log("setting view");
 
+    // remove current user marker
+    if (userPositionMarkerRef.current)
+      bikeMapRef.current.removeLayer(userPositionMarkerRef.current);
+
     bikeMapRef.current.setView(props.userPosition, 18);
+
+    // make icon
     const userPositionIcon = L.icon({
       iconUrl: userPositionMobileSvg,
       iconSize: [56, 56],
     });
 
-    // remove previous marker!
-    L.marker(props.userPosition, { icon: userPositionIcon }).addTo(
-      bikeMapRef.current
-    );
+    // make marker
+
+    userPositionMarkerRef.current = L.marker(props.userPosition, {
+      icon: userPositionIcon,
+    });
+
+    // add marker
+
+    userPositionMarkerRef.current.addTo(bikeMapRef.current);
+
     console.log("added marker");
   });
 
@@ -66,10 +79,13 @@ function Main(props) {
   const TAIPEI_COORDINATES = [25.03746, 121.564558];
   const [userPosition, setUserPosition] = useState(TAIPEI_COORDINATES);
   const [bikesAvailable, setBikesAvailable] = useState([]);
+  const [showLocatingMessage, setShowLocatingMessage] = useState(false);
+
   async function getUserPosition() {
     try {
-      /* handle getting location message */
+      setShowLocatingMessage(true);
       const userCoordinates = await asyncGetGeolocation();
+      setShowLocatingMessage(false);
       console.log(userCoordinates);
       setUserPosition(userCoordinates);
     } catch (error) {
@@ -77,11 +93,11 @@ function Main(props) {
     }
   }
 
-  useEffect(() => {
-    // find User position on first render
-    console.log("find User position on first render");
-    getUserPosition();
-  }, []);
+  //   useEffect(() => {
+  //     // find User position on first render
+  //     console.log("find User position on first render");
+  //     getUserPosition();
+  //   }, []);
 
   const API_KEY = {
     APP_ID: process.env.REACT_APP_APP_ID,
@@ -141,6 +157,7 @@ function Main(props) {
             <MapInfo
               handleLocateUser={getUserPosition}
               bikesAvailable={bikesAvailable}
+              showLocatingMessage={showLocatingMessage}
             />
           }
         />
@@ -169,7 +186,7 @@ function Main(props) {
   );
 }
 
-function MapInfo({ handleLocateUser, bikesAvailable }) {
+function MapInfo({ handleLocateUser, bikesAvailable, showLocatingMessage }) {
   const [expandResultList, setExpandResultList] = useState(false);
 
   function handleExpandResultList() {
@@ -182,6 +199,8 @@ function MapInfo({ handleLocateUser, bikesAvailable }) {
     <img src={collapseTopSvg} alt="collapse top icon" />
   );
 
+  // implement sort
+
   const results = bikesAvailable.map((station) => {
     const stationStatusText =
       station.serviceStatus !== 1
@@ -191,6 +210,40 @@ function MapInfo({ handleLocateUser, bikesAvailable }) {
         : station.availableReturnBikes > 0
         ? "只可停車"
         : "只可借車";
+    const stationStatusStyle =
+      station.serviceStatus !== 1
+        ? "off"
+        : station.availableRentBikes === 0 || station.availableReturnBikes === 0
+        ? "limited"
+        : "";
+    const availableBikesStyle =
+      station.availableRentBikes === 0
+        ? "none"
+        : station.availableRentBikes <= 5
+        ? "few"
+        : "";
+    const availableParksStyle =
+      station.availableReturnBikes === 0
+        ? "none"
+        : station.availableReturnBikes <= 5
+        ? "few"
+        : "";
+    const availableBikesImg =
+      station.availableRentBikes === 0 ? (
+        <img src={bicycleGreySvg} alt="bicycle icon" />
+      ) : station.availableRentBikes <= 5 ? (
+        <img src={bicycleRedSvg} alt="bicycle icon" />
+      ) : (
+        <img src={bicycle500Svg} alt="bicycle icon" />
+      );
+    const availableParksImg =
+      station.availableReturnBikes === 0 ? (
+        <img src={parkingGreySvg} alt="parking icon" />
+      ) : station.availableReturnBikes <= 5 ? (
+        <img src={parkingRedSvg} alt="parking icon" />
+      ) : (
+        <img src={parkingSvg} alt="parking icon" />
+      );
     const updateTime = /.*T(\d*:\d*)/g.exec(station.srcUpdateTime)[1];
     return (
       <div className="bike_result" key={station.stationId}>
@@ -198,24 +251,25 @@ function MapInfo({ handleLocateUser, bikesAvailable }) {
           <h3 className="typography-bold typography-button">
             {station.stationName}
           </h3>
-          <span className="status typography-medium typography-caption">
+          <span
+            className={`status ${stationStatusStyle} typography-medium typography-caption`}
+          >
             {stationStatusText}
           </span>
           <span className="distance typography-medium typography-caption">
-            <img src={mapMarkerGreySvg} alt="map marker icon" />
             {`於${updateTime}更新`}
           </span>
         </div>
         <div className="available">
-          <div className="available_bikes">
-            <img src={bicycle500Svg} alt="bicycle icon" />
+          <div className={`available_bikes ${availableBikesStyle}`}>
+            {availableBikesImg}
             <span className="typography-medium typography-button">可租借</span>
             <span className="quantity typography-bold typography-h5">
               {station.availableRentBikes}
             </span>
           </div>
-          <div className="available_parks">
-            <img src={parkingRedSvg} alt="parking icon" />
+          <div className={`available_parks ${availableParksStyle}`}>
+            {availableParksImg}
             <span className="typography-medium typography-button">可停車</span>
             <span className="quantity typography-bold typography-h5">
               {station.availableReturnBikes}
@@ -242,6 +296,11 @@ function MapInfo({ handleLocateUser, bikesAvailable }) {
           找車位
         </button>
       </div>
+      {showLocatingMessage ? (
+        <div className="locating_message">
+          <span className="typography-bold typography-h4">定位中</span>
+        </div>
+      ) : null}
       <div className={`results_list ${expandResultList ? "expand" : ""}`}>
         <button className="geolocation" onClick={handleLocateUser}>
           <img src={geolocactionSvg} alt="geo location icon" />
@@ -250,16 +309,24 @@ function MapInfo({ handleLocateUser, bikesAvailable }) {
           {collapseImg}
         </div>
         <div className="filter">
-          <input
-            className="typography-medium typography-caption"
-            type="search"
-            placeholder="搜尋站點或鄰近地點"
-            value=""
-          />
-          <button className="sort typography-bold typography-caption">
-            <img src={sortSvg} alt="sort icon" />
-            排序
-          </button>
+          {results.length ? (
+            <>
+              <input
+                className="typography-medium typography-caption"
+                type="search"
+                placeholder="搜尋站點或鄰近地點"
+                value=""
+              />
+              <button className="sort typography-bold typography-caption">
+                <img src={sortSvg} alt="sort icon" />
+                排序
+              </button>
+            </>
+          ) : (
+            <h3 className="no_results typography-bold typography-h5">
+              一公里內沒有youbike車站
+            </h3>
+          )}
         </div>
         <div className="results">{results}</div>
       </div>
